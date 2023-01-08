43080b9a4fe9c7c958aadbe92fbc834c0975721c
ArtPryw <artur.pryw@gmail.com>
2023-01-08 21:39:58 +0100

    Added method checking allels cols location

--- core.py
+++ core.py
@@ -24,7 +24,9 @@ class FileInfoGetter():
         counter = Counter(found_separators)
         separator = counter.most_common()
         separator = separator[0][0]
-
+        if separator == '	':
+            separator = '\t'
+        print(f"Separator = {separator}")
         return separator
 
     def __check_skiprow(self):
@@ -41,16 +43,49 @@ class FileInfoGetter():
                         break
                     else:
                         skiprows += 1
+            print(f"Skiprows = {skiprows}")
             return skiprows
+
+    def __location_of_allels_cols(self):
+        separator = self.__check_separator()
+        skiprows = self.__check_skiprow()
+
+        df = pd.read_csv(self.file_path, skiprows=skiprows, nrows=10, sep=separator)
+        translated_allels = ['B']
+        not_translated_allels = ['A', 'C', 'T', 'G']
+
+        translated_alleles_col_postion_list = []
+        not_translated_alleles_col_postion_list = []
+        for i in range(len(df.columns)):
+            col_to_list = df.iloc[:, i].tolist()
+            if any(element in translated_allels for element in col_to_list):
+                translated_alleles_col_postion_list.append(i)
+        if len(translated_alleles_col_postion_list) == 0:
+            for i in range(len(df.columns)):
+                col_to_list = df.iloc[:, i].tolist()
+                if any(element in not_translated_allels for element in col_to_list):
+                    not_translated_alleles_col_postion_list.append(i)
+            result = not_translated_alleles_col_postion_list
+        else:
+            result = translated_alleles_col_postion_list
+
+        return tuple(result)
+
+
 
     def build_dict(self):
 
         separator = self.__check_separator()
         skiprows = self.__check_skiprow()
-        dict = {"separator": separator, "skiprows": skiprows}
+        location_of_allels_cols = self.__location_of_allels_cols()
+        dict = {"separator": separator, "skiprows": skiprows, "location_of_allels_cols": location_of_allels_cols}
         return dict
 
 def source_file_load(file):
-    source_file = pd.read_csv(file, skiprows = 10)
+
+    info_getter = FileInfoGetter(file)
+    df_information = info_getter.build_dict()
+
+    source_file = pd.read_csv(file, skiprows=df_information['skiprows'], sep=df_information['separator'])
 
     return source_file
--- test_etl.py
+++ test_etl.py
@@ -3,20 +3,23 @@ import pandas as pd
 import pandas.testing as pd_testing
 from core import *
 
+file = 'test_data/test2_csv.txt'
+skiprows = 11
+separator = '\t'
+location_of_allels_cols = (2,3,4,5)
 
-skiprows = 10
 class TestStrFind(unittest.TestCase):
 
     def test_source_file_load(self):
 
-        test_data = pd.read_csv('test_data/test_csv.csv', skiprows = skiprows)
-        pd_testing.assert_frame_equal(source_file_load('test_data/test_csv.csv'), test_data)
+        test_data = pd.read_csv(file, skiprows=skiprows, sep=separator)
+        pd_testing.assert_frame_equal(source_file_load(file), test_data)
 
     def test_get_info_about_file(self):
 
-        test_data = 'test_data/test_csv.csv'
+        test_data = file
 
-        dict = {"separator": ",", "skiprows":10}
+        dict = {"separator": separator, "skiprows":skiprows, "location_of_allels_cols":location_of_allels_cols}
         file_info_getter_dict = FileInfoGetter(test_data).build_dict()
         print(file_info_getter_dict)
         self.assertDictEqual(dict, file_info_getter_dict)
